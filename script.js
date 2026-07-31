138c5820d5d9b64ea9977cc5156c28d1015aac3d
Raunak Singh Negi <negiraunak49@gmail.com>
2026-07-31 23:29:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+    const now = new Date();
+
+    document.getElementById("india").textContent =
+        now.toLocaleString("en-IN", {
+            timeZone: "Asia/Kolkata",
+            dateStyle: "medium",
+            timeStyle: "medium"
+        });
+
+    document.getElementById("east").textContent =
+        now.toLocaleString("en-US", {
+            timeZone: "America/New_York",
+            dateStyle: "medium",
+            timeStyle: "medium"
+        });
+
+    document.getElementById("west").textContent =
+        now.toLocaleString("en-US", {
+            timeZone: "America/Los_Angeles",
+            dateStyle: "medium",
+            timeStyle: "medium"
+        });
 }
+
 showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+setInterval(showTime, 1000);
